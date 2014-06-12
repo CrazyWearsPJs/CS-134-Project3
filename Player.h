a1cec54498546981b78a7edca4c16bd9b46d7097
Leumash <shong010@ucr.edu>
2014-06-12 02:19:06 -0700

    Attempting Pitch

--- Player.h
+++ Player.h
@@ -3,17 +3,25 @@
 #include "GameEntity.h"
 #include "Projectile.h"
 #include <vector>
+
+using namespace Ogre;
+
 class Player : public GameEntity{
 
 	private:
 		std::vector<Ogre::String> collision(Ogre::RaySceneQuery *);
+        Radian pitch;
 
 	public:
 		Player(const Ogre::String &, Ogre::Vector3, Ogre::Vector3);
 
 		Projectile * fireProjectile(Ogre::SceneManager *);
 
+        void moveTo(Vector3 pos, SceneManager * manager);
+
 		void move(Ogre::SceneManager *, Ogre::RaySceneQuery *, Ogre::Camera *);
+
+        void rotatePitch(SceneManager * manager);
 
 		~Player();
 
